60289d889588c8faae6337d3d602b26cf9f1a91c
gdiaz38 <gdiaz38@ucmerced.edu>
2022-02-14 17:57:54 -0800

    updated

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Gabriel Diaz on 2/12/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
